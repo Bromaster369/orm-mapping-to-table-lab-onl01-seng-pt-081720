4f5e91e253adf75770929d9e76767f39106d89ae
Christian Pardo <chrispardopersonal@gmail.com>
2020-10-26 22:56:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,19 @@
-class Student
-
-  # Remember, you can access your database connection anywhere in this class
+# Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
+class Student
+  
+  attr_accessor :name, :grade
+  
+  def initialize(name, grade, id = nil)
+    @name = name
+    @grade = grade 
+    @id = id 
+    
+  end 
+
+  def self.create_table
+  
+  
+  end 
   
 end
