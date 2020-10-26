a6d4412f7113ac6a3499ae7e91807a70ddbe570d
Christian Pardo <chrispardopersonal@gmail.com>
2020-10-26 22:59:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,8 +18,8 @@ class Student
       CREATE TABLE IF NO EXISTS students (
       id INTEGER PRIMARY KEY,
       name TEXT,
-      grade INTEGER)
-    
+      grade INTEGER
+      )
     SQL
   
   end 
