4febe0137d1cd240b554601cda6fbb9bebfb4a4e
Christian Pardo <chrispardopersonal@gmail.com>
2020-10-26 23:09:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,5 @@
 # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
-  require 'pry'
 class Student
   
   attr_accessor :name, :grade
@@ -45,7 +44,7 @@ class Student
       
     DB[:conn].execute(sql, self.name, self.grade)
     
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")binding.pry
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     
   end 
   
